af042f97dbd98d2d1b39f4aa754c2be30ec4204b
岸田　理沙 <kjs03180_0104@yahoo.co.jp>
2021-04-02 01:02:13 +0900

    master repository

--- database/migrations/2021_03_18_132724_create_cart_table.php
+++ database/migrations/2021_03_18_132724_create_cart_table.php
@@ -15,6 +15,8 @@ class CreateCartTable extends Migration
     {
         Schema::create('cart', function (Blueprint $table) {
             $table->id();
+            $table->integer('user_id');
+            $table->foreign('user_id')->references('id')->on('users');
             $table->string('sub_name');
             $table->text('sub_details')->nullable();
             $table->unsignedInteger('sub_price');
